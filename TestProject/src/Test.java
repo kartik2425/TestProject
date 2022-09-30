55e45fac32dfc6a11ea575eaf197aa59a8d6d001
Harsha <Harsha@DESKTOP-SCEQ7CP>
2022-09-30 09:59:24 +0530

    first pgm Test.java

---
+++ TestProject/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test {
+	
+	public static void main(String [] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println("sum is "+(a+b));
+	}
+
+}
